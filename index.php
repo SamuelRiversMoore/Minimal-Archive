78f378c107dbf67d3910525115ef60dd51a93534
t as in ttttttt <repertttttttoire@gmail.com>
2015-09-10 01:59:00 +0100

    Initial commit
    - added keyboard nav (left, right, esc)
    - php cosmetics
    - split css and js from html

--- index.php
+++ index.php
@@ -1,87 +1,41 @@
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html">
-<title>Show images in folder</title>
-<style type="text/css">
-body {
-    margin: 10px 0px 10px 10px;
-    padding: 0;
-    background: #fff;
-    text-align: left;
-}
-.legend {
-    padding: 0 0 5px;
-    text-align: left;
-    font: 10px sans-serif;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	white-space: nowrap;
-}
-.container {
-    width: 100%;
-}
-.img {
-	display: inline-block;
-	vertical-align: top;
-	margin: 0px 10px 10px 0px;
-	text-align: center;
-}
-.small {
-	max-width: 150px;
-	max-height: 150px;
-}
-.large {
-	max-width: 100% !important;
-	max-height: auto;
-}
-img {
-    display: inline-block;
-    margin: 0px auto 5px;
-	max-width: 100%;
-	height: auto;
-	cursor: pointer;
-}
-.small>img{	max-height: 83%;}
-
-</style>
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-</head>
-<body>
- <!-- <div id="header">Title</div> -->
 <?php
-$folder = 'img/';
-$filetype = '*.*';
-$files = glob($folder.$filetype);
-$count = count($files);
- 
-$sortedArray = array();
-for ($i = 0; $i < $count; $i++) {
-    $sortedArray[date ('YmdHis', filemtime($files[$i]))] = $files[$i];
-}
- 
-krsort($sortedArray);
-
-echo '<div id="container">';
-foreach ($sortedArray as &$filename) {
-    echo '<div class="img small">';
-    echo '<img src="'.$filename.'" />';
-    echo '<div class="legend" title="'.substr($filename,strlen($folder),strpos($filename, '.')-strlen($folder)).'">';
-    echo substr($filename,strlen($folder),strpos($filename, '.')-strlen($folder));
-    echo '</div>';
-    echo '</div>';
-}
-echo '</div>';
+	$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+	$file = '*';
+	$dir = 'assets/img/'; //put images here
 ?>
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Show images in folder</title>
+		<link rel="stylesheet" href="<?php echo $baseUrl ?>assets/css/style.css" type="text/css" media="screen"/>
+	</head>
+	<body>
+		<header>
+		    <section class="title">Minimal Archive</section>
+		</header>
+		<main>
+		    <?php
 
-<script type="text/javascript">
-	$('div.img').on('click', function (event) {
-	if ($(this).hasClass('small')){
-			$('.large').removeClass('large').addClass('small');
-			$(this).removeClass('small').addClass('large');
-		} else {
-			$(this).removeClass('large').addClass('small');
-		}
-	});
-</script>
+	        // Open the directory, and read its contents
+	        if (is_dir($dir)){
+	            $files = scandir($dir);
+	            foreach ($files as $file){
+	                if ($file != "." && $file != "..") {
+	                    echo "<div class='img small'>";
+	                    echo "<img class='lazy' src='" . $baseUrl . "assets/css/loading.gif' data-original='" . $baseUrl . $dir . $file ."' title='" . $file . "'/>";
+	                    echo "<div class='caption'>" . $file . "</div>";
+	                    echo "</div>";
+	                }
+	            }
+	        }
 
-</body>
+	        ?>
+		    <span id="breaker"></span>
+		</main>
+		<footer>
+		</footer>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js"></script>
+		<script src="<?php echo $baseUrl ?>assets/js/main.js"></script>
+	</body>
+</html>
