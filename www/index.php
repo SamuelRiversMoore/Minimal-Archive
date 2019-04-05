3badf0f3f404580858326a4cf67c338fe7fb7a87
Tayeb B <repertttttttoire@gmail.com>
2019-04-05 12:51:08 +0200

    Index.php variable checks

--- www/index.php
+++ www/index.php
@@ -3,22 +3,22 @@
 	define('LIB', './lib/');
 	include_once LIB . 'functions.php';
 
-    /**
-     * Image folder name
-     * @var string
-     */
-    $dir = 'images';
     $meta = textFileToArray('./meta.txt');
+    $imagesdir = array_key_exists('imagesfolder', $meta) ? $meta['imagesfolder'] : null;
+    $title = array_key_exists('title', $meta) ? $meta['title'] : '';
+    $description = array_key_exists('description', $meta) ? $meta['description'] : '';
+    $socialimage = array_key_exists('socialimage', $meta) ? $meta['socialimage'] : '';
+    $note = array_key_exists('note', $meta) ? $meta['note'] : '';
 ?>
 <html>
 	<head>
     	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    	<title><?= $meta['title']; ?></title>
-        <meta name="description" content="<?= $meta['description']; ?>" />
+    	<title><?= $title; ?></title>
+        <meta name="description" content="<?= $description ?>" />
 
-        <meta property="og:title" content="<?= $meta['title'] ?>">
-        <meta property="og:description" content="<?= $meta['description'] ?>">
-        <meta property="og:image" content="<?= url($meta['socialimage']) ?>">
+        <meta property="og:title" content="<?= $title ?>">
+        <meta property="og:description" content="<?= $description ?>">
+        <meta property="og:image" content="<?= url($socialimage) ?>">
         <meta property="og:url" content="<?= url() ?>">
 
     	<link rel="stylesheet" href="<?php echo url('assets/css/style.css')?>" type="text/css" media="screen"/>
@@ -26,16 +26,16 @@
 	</head>
 	<body>
 		<header>
-		    <section class="title"><?= $meta['title']; ?></section>
+		    <section class="title"><?= $title ?></section>
 		</header>
 		<main>
             <section class="Gallery">
                 <?php
-                $images = getImagesInFolder($dir);
+                $images = getImagesInFolder($imagesdir);
                 foreach ($images as $image){
                     $output = "<div class='Image'>";
                     $output .= "<div class='Image__container'>";
-                    $output .= "<img class='lazy miniarch' src='" . url('assets/css/loading.gif') . "' data-src='" .  url("${dir}/${image}") ."' title='" . $image . "'/>";
+                    $output .= "<img class='lazy miniarch' src='" . url('assets/css/loading.gif') . "' data-src='" .  url("${imagesdir}/${image}") ."' title='" . $image . "'/>";
                     $output .= "</div>";
                     $output .= "<div class='Image__caption'><span>" . $image . "</span></div>";
                     $output .= "</div>";
@@ -48,7 +48,7 @@
 		</main>
 		<footer>
             <section class="note">
-                <?= $meta['note'] ?>
+                <?= $note ?>
             </section>
 		</footer>
 		<script src="<?php echo url('assets/js/main.js')?>"></script>
