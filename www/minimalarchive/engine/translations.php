003a1297e08f390b553f326f398b180caa36586b
Tayeb B <repertttttttoire@gmail.com>
2019-04-12 15:09:28 +0200

    Updated translations

--- www/minimalarchive/engine/translations.php
+++ www/minimalarchive/engine/translations.php
@@ -5,7 +5,7 @@ return [
         'account_exists'    => "There is already an account, you must delete it to create a new one.",
         'bad_credentials'   => 'Bad credentials...',
         'installation_complete' => "✅ Installation is complete!",
-        'no_folder'         => "No folder",
+        'no_folder'         => "The folder doesn't exist",
         'password_short'    => "🙁 Password too short! you need at least 8 characters.",
         'password_bad'      => '🙁 The password must contain at least one digit and at least one uppercase letter.',
         'uninstall_complete'    => "✅ Successfully uninstalled the project"
@@ -13,8 +13,11 @@ return [
     'fr' => [
         'account_created'   => "😀 Le compte a été crée",
         'account_exists'    => "Un compte a déjà été crée. Pour en créer un nouveau, veuillez supprimer l'ancien.",
+        'bad_credentials'   => 'Mauvais identifiants...',
         'installation_complete' => "✅ L'installation est terminée!",
+        'no_folder'         => "Le dossier n'existe pas...",
         'password_short'    => "🙁 Mot de passe trop court! 8 caractères au minimum.",
-        'password_bad'      => '🙁 Le mot de passe doit contenir au moins un chiffre et une lettre majuscule.'
+        'password_bad'      => '🙁 Le mot de passe doit contenir au moins un chiffre et une lettre majuscule.',
+        'uninstall_complete'    => "✅ Le projet a été désinstallé."
     ],
 ];
