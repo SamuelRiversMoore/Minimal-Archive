dd0cc57920f5df1cdd0ae53b7d40dbe55e52e266
Tayeb B <repertttttttoire@gmail.com>
2019-05-19 01:40:31 +0200

    Update build

--- www/assets/js/edit.js
+++ www/assets/js/edit.js
@@ -9,6 +9,8 @@
   const SELECTOR_NOTE = '.note';
 
   const EVENT_RESET = 'reset';
+  const EVENT_LOADED = 'loaded';
+  const EVENT_LOADING = 'loading';
   const EVENT_IMAGE_UPDATE = 'image-update';
 
   function ProgressBar (progressBarSelector) {
@@ -297,9 +299,11 @@
         data.note = stripHtmlTags(note.innerHTML);
       }
 
+      document.dispatchEvent(new Event(EVENT_LOADING));
       this.saveMeta(data, this.getCsrfToken(this.saveBtn))
         .then(res => console.log(res))
-        .catch(err => console.log(err));
+        .catch(err => console.log(err))
+        .finally(() => document.dispatchEvent(new Event(EVENT_LOADED)));
     }
 
     previewChanges () {
@@ -1041,7 +1045,74 @@
     }
   }
 
+  const mergeSettings$2 = (options) => {
+    const settings = {
+      selector: 'Loader'
+    };
+
+    for (const attrName in options) {
+      settings[attrName] = options[attrName];
+    }
+
+    return settings
+  };
+
+  class Loader {
+    constructor (options) {
+      this.config = mergeSettings$2(options);
+      this.init();
+    }
+
+    init () {
+      const {
+        selector
+      } = this.config;
+      const content = document.createElement('div');
+
+      content.classList.add('content');
+      content.innerHTML = 'Loading...';
+
+      this.dom = document.createElement('aside');
+      this.dom.classList.add(selector);
+      this.dom.appendChild(content);
+
+      console.log(this.dom);
+      document.body.appendChild(this.dom);
+      this.initListeners();
+    }
+
+    initListeners () {
+      document.addEventListener(EVENT_LOADING, () => {
+        this.start();
+      });
+      document.addEventListener(EVENT_LOADED, () => {
+        setTimeout(() => {
+          this.stop();
+        }, 1000);
+      });
+    }
+
+    start () {
+      this.dom.classList.remove('transition-end');
+      this.dom.classList.remove(EVENT_LOADED);
+      this.dom.classList.add('transition-start');
+      setTimeout(() => {
+        this.dom.classList.add(EVENT_LOADING);
+      }, 0.25);
+    }
+
+    stop () {
+      this.dom.classList.remove('transition-start');
+      this.dom.classList.remove(EVENT_LOADING);
+      this.dom.classList.add(EVENT_LOADED);
+      setTimeout(() => {
+        this.dom.classList.add('transition-end');
+      }, 0.25);
+    }
+  }
+
   document.addEventListener('DOMContentLoaded', () => {
+    (() => new Loader())();
     const gallery = new Gallery({
       gallerySelector: '.Gallery',
       imageSelector: '.Image',
