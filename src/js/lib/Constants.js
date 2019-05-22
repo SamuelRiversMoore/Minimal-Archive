2d17467772a9416028bdba5d08923a5742ef01f3
Tayeb B <repertttttttoire@gmail.com>
2019-05-22 03:03:59 +0200

    Add constants

--- src/js/lib/Constants.js
+++ src/js/lib/Constants.js
@@ -4,8 +4,10 @@ export const API_SAVE = 'save'
 
 export const SELECTOR_TITLE = '.title'
 export const SELECTOR_NOTE = '.note'
+export const SELECTOR_IMAGE = '.Image__container img'
 
 export const EVENT_RESET = 'reset'
+export const EVENT_STATUS_CHANGE = 'status-change'
 export const EVENT_LOADED = 'loaded'
 export const EVENT_LOADING = 'loading'
 export const EVENT_IMAGE_UPDATE = 'image-update'
