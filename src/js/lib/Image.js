3955a248125b5b15e39a7f05f3864df57543bc5c
Tayeb B <repertttttttoire@gmail.com>
2019-05-22 03:02:01 +0200

    Add methods
    filename, id, src, added constants

--- src/js/lib/Image.js
+++ src/js/lib/Image.js
@@ -1,22 +1,39 @@
 import {
-  isDomNode
+  isDomNode,
+  uuidv4
 } from './Helpers.js'
+import {
+  EVENT_RESET,
+  EVENT_STATUS_CHANGE,
+  EVENT_IMAGE_UPDATE
+} from './Constants.js'
 
 class Image {
   constructor (image) {
     if (!isDomNode(image)) {
-      console.error('%o is not a dom element!', image)
+      console.error('%o is not a dom element! aborting image creation', image)
+    } else {
+      this.id = uuidv4()
+      this.dom = image
+      this.url = image.querySelector('img').src
+
+      const datafilename = image.querySelector('img').getAttribute('data-filename')
+      if (datafilename) {
+        this.file = datafilename.substring(datafilename.lastIndexOf('/') + 1)
+      } else {
+        this.file = image.querySelector('img').src.substring(image.querySelector('img').src.lastIndexOf('/') + 1)
+      }
+
+      this.stat = false
+      this.initListeners()
+      this.dispatchStatusUpdate = this.dispatchStatusUpdate.bind(this)
     }
-    this.dom = image
-    this.stat = false
-    this.initListeners()
-    this.dispatchStatusUpdate = this.dispatchStatusUpdate.bind(this)
   }
 
   initListeners () {
     this.dom.addEventListener('click', this.toggleStatus.bind(this))
-    this.dom.addEventListener('status-change', this.applyStyle.bind(this))
-    document.addEventListener('reset', (e) => {
+    this.dom.addEventListener(EVENT_STATUS_CHANGE, this.applyStyle.bind(this))
+    document.addEventListener(EVENT_RESET, (e) => {
       this.stat = false
       this.dispatchStatusUpdate()
     })
@@ -28,8 +45,8 @@ class Image {
   }
 
   dispatchStatusUpdate (event) {
-    this.dom.dispatchEvent(new Event('status-change'))
-    document.dispatchEvent(new CustomEvent('image-update', {
+    this.dom.dispatchEvent(new Event(EVENT_STATUS_CHANGE))
+    document.dispatchEvent(new CustomEvent(EVENT_IMAGE_UPDATE, {
       detail: {
         image: this.stat ? this : null
       }
@@ -44,6 +61,10 @@ class Image {
     }
   }
 
+  getId () {
+    return this.id
+  }
+
   set image (image) {
     this.dom = image
   }
@@ -54,11 +75,26 @@ class Image {
 
   set status (status) {
     this.stat = status
-    this.dom.dispatchEvent(new Event('status-change'))
+    this.dom.dispatchEvent(new Event(EVENT_STATUS_CHANGE))
   }
 
   get status () {
     return this.stat
+  }
+
+  set filename (filename) {
+    this.file = filename
+  }
+
+  get filename () {
+    return this.file
+  }
+
+  set src (src) {
+    this.url = src
+  }
+  get src () {
+    return this.url
   }
 }
 
